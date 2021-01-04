f531aa8d7e3f748ee68c3c93ba4e6a5c18bf0269
iPolosatij <9mihail@gmail.com>
2021-01-04 12:09:39 +0300

    complit

--- app/src/main/java/space/digitallab/testappforinstat/PlayVideo.java
+++ app/src/main/java/space/digitallab/testappforinstat/PlayVideo.java
@@ -1,33 +1,17 @@
 package space.digitallab.testappforinstat;
 
 import android.content.pm.ActivityInfo;
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.net.Uri;
 import android.widget.MediaController;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.VideoView;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-import space.digitallab.testappforinstat.dto.VideoUrls;
-import space.digitallab.testappforinstat.net.Adapter;
-import space.digitallab.testappforinstat.net.BodyUrlResponse;
-import space.digitallab.testappforinstat.net.DataCall;
 
 public class PlayVideo extends AppCompatActivity {
 
-    private  TextView textView;
+    private  VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +20,12 @@ public class PlayVideo extends AppCompatActivity {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        textView = findViewById(R.id.textUrl);
-        textView.setText(getIntent().getStringExtra("url"));
+        videoView = findViewById(R.id.myVideo);
+
+        String vidAddress = getIntent().getStringExtra("url");
+        Uri vidUri = Uri.parse(vidAddress);
+        videoView.setVideoURI(vidUri);
+        videoView.start();
 
     }
 }
